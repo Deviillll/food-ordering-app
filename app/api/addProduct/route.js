1b119c267237db6ee368b7f8c51cbeda7b79adc6
Hammad Hussain <93250@gcslahore.edu.pk>
2024-06-25 10:51:14 -0700

    Delete route.js

--- app/api/addProduct/route.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import connectDb from "../../_dbConnection/db";
-export async function GET (){
-  await connectDb();
- return NextResponse.json({message: "Hello World"},{status:200});
-}

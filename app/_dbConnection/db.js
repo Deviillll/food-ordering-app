b340b62371c0c9488a0ffa22f84161e4d9f5cba3
Hammad Hussain <93250@gcslahore.edu.pk>
2024-06-25 10:46:10 -0700

    fix env

--- app/_dbConnection/db.js
+++ app/_dbConnection/db.js
@@ -1,20 +1,20 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
 
-const connectDb = async () => {
-    const connection = mongoose.connection;
-    if(connection.readyState==1)
-    {
-        console.log("Already connected");
-        return;
-    }
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
-    console.log(mongoose.connection.readyState);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-}
-export default connectDb;
+// const connectDb = async () => {
+//     const connection = mongoose.connection;
+//     if(connection.readyState==1)
+//     {
+//         console.log("Already connected");
+//         return;
+//     }
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log("MongoDB connected");
+//     console.log(mongoose.connection.readyState);
+//   } catch (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+// }
+// export default connectDb;
